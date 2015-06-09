347756a286fe6360e9fcece84407ab2f46f00c2d
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-06-09 15:26:05 +1200

    Ensure this tool only supports IExposureFile.

--- src/pmr2/bives/utility.py
+++ src/pmr2/bives/utility.py
@@ -2,6 +2,7 @@ import zope.component
 from zope.interface import implementer
 
 from pmr2.app.exposure.interfaces import IExposureFileTool
+from pmr2.app.exposure.interfaces import IExposureFile
 
 
 @implementer(IExposureFileTool)
@@ -13,4 +14,10 @@ class ExposureFileComparisonTool(object):
     label = u'Compare...'
 
     def get_tool_link(self, exposure_object):
-        return exposure_object.absolute_url() + '/bives_pick_file'
+        # TODO figure out how to do user-side customization on the type
+        # of files that can be added.  Although this might better be
+        # done on the comparison page as that is the centralized place
+        # for all forms of comparison (i.e. CellML or SBML, etc).
+
+        if IExposureFile.providedBy(exposure_object):
+            return exposure_object.absolute_url() + '/bives_pick_file'
